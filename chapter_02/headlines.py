d6759d8abb0718b4be5e12c045f432995258f122
Joe Rowley (Desktop) <jrowley2@gmail.com>
2017-11-02 23:02:32 -0600

    Trying out POST input from the form

--- chapter_02/headlines.py
+++ chapter_02/headlines.py
@@ -21,9 +21,9 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def get_news():
-    query = request.args.get("publication")
+    query = request.form.get("publication")
     if not query or query.lower() not in RSS_FEEDS:
         publication = "bbc"
     else:
